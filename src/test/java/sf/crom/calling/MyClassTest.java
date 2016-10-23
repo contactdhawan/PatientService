b16cf172500bab5c5962f5b044e72c37e7e15cfd
neeraj <contactdhawan@gmail.com>
2016-10-23 01:55:28 -0500

    adding JUNITS

--- src/test/java/sf/crom/calling/MyClassTest.java
+++ src/test/java/sf/crom/calling/MyClassTest.java
@@ -1,10 +1,22 @@
 package sf.crom.calling;
 
-import org.junit.Test;
+import junit.framework.Assert;
 
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class MyClassTest {
-@Test
+String key="100";
+String value="200";
+	@Test
 	public void testAdd(){
-		
+		MyClass myclass = new MyClass();
+		myclass.add(key, value);
+	}
+	public void testGet(){
+		MyClass myclass = new MyClass();
+		String result = myclass.getValue(key);
+		Assert.assertEquals(value, result);
 	}
 }
