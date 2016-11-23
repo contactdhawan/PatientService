31f95e1e8e59eeaad2659a8bf11fadffd02eb130
neeraj <contactdhawan@gmail.com>
2016-11-22 22:30:56 -0600

    Initial commit

---
+++ src/main/java/sf/crom/service/PatientService.java
@@ -0,0 +1,17 @@
+package sf.crom.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+@Produces({ "application/json,application/xml" })
+public class PatientService {
+	@GET
+	@Path("/get/{key}")
+	public Response getValue(@PathParam("key") String key) {
+		return Response.ok().build();
+	}
+
+}
--- src/test/java/sf/crom/calling/MyClassTest.java
+++ src/test/java/sf/crom/calling/MyClassTest.java
@@ -1,13 +1,15 @@
 package sf.crom.calling;
 
+import java.util.Locale;
+
 import junit.framework.Assert;
 
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+
 public class MyClassTest {
-String key="100";
+/*String key="100";
 String value="200";
 	@Test
 	public void testAdd(){
@@ -18,5 +20,10 @@ String value="200";
 		MyClass myclass = new MyClass();
 		String result = myclass.getValue(key);
 		Assert.assertEquals(value, result);
+	}*/
+	static int x=5;
+	public static void main(){
+Locale l = new Locale("USA");
+		System.out.println(l.getCountry());
 	}
 }
