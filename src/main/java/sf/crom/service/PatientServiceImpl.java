5345b2d69a51defeccaae492c61c66ba0f7934d8
neeraj <contactdhawan@gmail.com>
2016-11-23 23:01:40 -0600

    enabling logging

--- src/main/java/sf/crom/service/PatientServiceImpl.java
+++ src/main/java/sf/crom/service/PatientServiceImpl.java
@@ -1,6 +1,7 @@
 package sf.crom.service;
 
 import java.util.HashMap;
+import java.util.logging.Logger;
 
 import sf.crom.bo.Patient;
 
@@ -22,6 +23,7 @@ public class PatientServiceImpl implements PatientService {
 
 	public Patient getPatient(String id) {
 		System.out.println("In get patient "+id);
+		Logger.getLogger("Ptient Service log").info("In get patient "+id);
 		Patient patient = patients.get(Long.parseLong(id));
 		return patient;
 	}
