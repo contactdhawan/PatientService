cec33bbbbe6d67e7c415d39e77ddba6c609bbf5b
neeraj <contactdhawan@gmail.com>
2016-11-24 01:25:54 -0600

    adding more junits

--- src/test/java/sf/crom/service/PatientServiceImplTest.java
+++ src/test/java/sf/crom/service/PatientServiceImplTest.java
@@ -1,6 +1,5 @@
 package sf.crom.service;
 
-
 import static org.junit.Assert.*;
 
 import javax.ws.rs.core.Response;
@@ -13,9 +12,9 @@ import sf.crom.bo.Patient;
 public class PatientServiceImplTest {
 	PatientServiceImpl patientServiceImpl;
 	Patient patient;
-	
+
 	@Before
-	public void setup(){
+	public void setup() {
 		patientServiceImpl = new PatientServiceImpl();
 		patient = new Patient();
 		patient.setName("Test Patient");
@@ -24,18 +23,37 @@ public class PatientServiceImplTest {
 	@Test
 	public void testCreatePatient() {
 		Response createPatientResp = patientServiceImpl.createPatient(patient);
-		assertEquals(200,createPatientResp.getStatus());
+		assertEquals(200, createPatientResp.getStatus());
 	}
 
 	@Test
-	public void testDeletePatient_should_return_200(){
-		Response deletePatientResp = patientServiceImpl.deletePatient("123");
-		assertEquals(200,deletePatientResp.getStatus());
+	public void testDeletePatient_should_return_200() {
+		Patient newPatient = new Patient();
+		newPatient.setName("Adding a patient to delete");
+		Response createdPatientResp = patientServiceImpl
+				.createPatient(newPatient);
+		Patient createdPatient = (Patient) createdPatientResp.getEntity();
+		Response deletePatientResp = patientServiceImpl
+				.deletePatient(createdPatient.getId() + "");
+		assertEquals(200, deletePatientResp.getStatus());
 	}
-	
+
 	@Test
-	public void testDeletePatient_should_return_304(){
+	public void testDeletePatient_should_return_304() {
 		Response deletePatientResp = patientServiceImpl.deletePatient("567");
-		assertEquals(304,deletePatientResp.getStatus());
+		assertEquals(304, deletePatientResp.getStatus());
+	}
+
+	@Test
+	public void testUpdatePatient() {
+		patient.setName("Updated Patient");
+		Response updatedPatientResp = patientServiceImpl.updatePatient(patient);
+		assertEquals(200, updatedPatientResp.getStatus());
+	}
+
+	@Test
+	public void testGetPatient() {
+		Patient getExistingPatient = patientServiceImpl.getPatient("123");
+		assertNotNull(getExistingPatient);
 	}
 }
